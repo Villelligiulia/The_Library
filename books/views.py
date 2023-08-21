1d50d319be471b7b48f218b1957622cdce5f68de
Villelligiulia <>
2023-08-21 17:45:34 +0000

    Create a book_list view to display the list of books imported in the database

--- books/views.py
+++ books/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Book, Category
 
 
 def book_list(request):
-    """ A view to return the index page """
+    book_queryset = Book.objects.all()
+    lowest_priced_books = Book.objects.order_by('price')[:5]
+    categories = Category.objects.all()
+    selected_category = request.GET.get('category')
+    show_picture = True
 
-    return render(request, 'books/book-list.html')
+    if selected_category:
+        books = Book.objects.filter(category__name=selected_category)
+    else:
+        books = book_queryset
+
+    context = {
+        'books': books,
+        'lowest_priced_books': lowest_priced_books,
+        'categories': categories,
+        'selected_category': selected_category,
+        'show_picture': show_picture,
+
+
+    }
+
+    return render(request, 'books/book_list.html', context)
