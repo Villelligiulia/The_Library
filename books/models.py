664f6c8bbc65f69cd10ba2beb5704038ab8050ba
Villelligiulia <>
2023-08-21 16:24:43 +0000

    Create Author, Category, Book and Review model in books app

--- books/models.py
+++ books/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(
+        upload_to='category_images', default='default_category.jpg')
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200, default='Untitled')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
+    cover_image = models.ImageField(
+        upload_to='book_covers', default='default_cover.jpg')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    ratings = models.DecimalField(max_digits=3, decimal_places=1, default=0.0)
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # Reference the Book model
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Review by {self.user.username} for {self.book.title}"
