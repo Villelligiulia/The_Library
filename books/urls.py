6fd8d789bfa00b9828eb4b0d1899b846e9dbfb92
Villelligiulia <>
2023-08-23 09:25:00 +0000

    Add carousel feature to best-seller.html

--- books/urls.py
+++ books/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('search-book/', views.search_book, name='search_book'),
     path('book/<int:book_id>/', views.book_detail, name='book_detail'),
     path('all_categories/', views.all_categories, name='all_categories'),
+    path('best-sellers/', views.best_sellers, name='best_sellers'),
 ]
