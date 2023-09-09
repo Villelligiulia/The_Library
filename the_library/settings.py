ec263a8942c2b1dfea5e2adfb50379a840bb9aea
Villelligiulia <>
2023-09-09 11:59:03 +0000

    Add media_url media_root in settings

--- the_library/settings.py
+++ the_library/settings.py
@@ -188,6 +188,9 @@ STATIC_URL = '/static/'
 STATIC_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Bucket Config
 if 'USE_AWS' in os.environ:
     # Cache control
@@ -195,7 +198,7 @@ if 'USE_AWS' in os.environ:
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
-    
+
     AWS_STORAGE_BUCKET_NAME = 'the--library-3ca77daf8ee2'
     AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
