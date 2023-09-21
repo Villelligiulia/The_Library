c650db3a96da7b204b07c64d61ad96db6b36ee72
Villelligiulia <>
2023-09-21 08:02:46 +0000

    deployment

--- books/forms.py
+++ books/forms.py
@@ -14,11 +14,11 @@ class ReviewForm(forms.ModelForm):
 
         }
 
-    def clean_rating(self):
-        rating = self.cleaned_data['rating']
-        if rating < 1 or rating > 5:
-            raise forms.ValidationError("Rating must be between 1 and 5.")
-        return rating
+    # def clean_rating(self):
+    #     rating = self.cleaned_data['rating']
+    #     if rating < 1 or rating > 5:
+    #         raise forms.ValidationError("Rating must be between 1 and 5.")
+    #     return rating
 
 
 class BookForm(forms.ModelForm):
